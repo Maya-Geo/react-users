28b872b2f7d46ce0f10db21da12bbcf0bf7d3c20
Maya Hagan <miyaclessa@gmail.com>
2021-07-18 00:39:13 +1200

    add UserList.jsx

---
+++ src/Components/UserList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import User from "./User"
+
+const UserList = ({users,deleteUser,editUser}) => {
+
+    const userList = users.map (
+        (item) => {
+          return (
+            
+           <User 
+           user ={item} 
+           deleteUser = {deleteUser}
+           editUser = {editUser}
+           />
+           
+          )
+        }
+     )
+     return <div>{userList}</div>
+         
+    
+   
+}
+
+export default UserList;
