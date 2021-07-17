a5fbed23cc137bd1c6bb4b3585935f7ea46a8465
Maya Hagan <miyaclessa@gmail.com>
2021-07-18 00:42:44 +1200

    add edit and delete

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import EditForm from "./EditForm"
+import Modal from "react-bootstrap/Modal";
+import Button from 'react-bootstrap/Button';
 
-const User = ({ user, index }) => {
+const User = ({user,deleteUser, editUser}) => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleDelete = (event)=> {
+        deleteUser(user.id)
+    }
+
+
     return (
-        <div>
-            <div className="user" key={index}>
+        <div style = {{textAlign: "center"}}>
+            <div className="user">
             <h3>Name: {user.name}</h3>
             <p>Email: {user.email}</p>
-            <p>Gen: {user.gen}</p>
-            <hr />
+            <p>Number: {user.number}</p>
+
+            <Button variant="primary" onClick={handleShow}>Edit</Button>{' '} 
+            <br></br>
+            
+           <Button variant="secondary" onClick ={handleDelete}>Delete</Button>{' '}
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <EditForm
+            user={user}
+            editUser={editUser}
+            closeModal={handleClose}
+          />
+        </Modal.Body>
+      </Modal>
+
         </div>
+
+        
     );
 }
 
