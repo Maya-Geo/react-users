004c01a6802a1397d808a800a0f773bef269e0a1
Maya Hagan <miyaclessa@gmail.com>
2021-08-18 10:41:06 +1200

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Store from './Store/Store';
+import { Provider } from  'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
+  <Provider store = {Store}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
