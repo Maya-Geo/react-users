fe1ac062ded0638ecb4bdb9dc50b1b9beb63f532
Maya Hagan <miyaclessa@gmail.com>
2021-08-26 08:42:30 +1200

    edit

--- src/Actions/UserAction.jsx
+++ src/Actions/UserAction.jsx
@@ -5,3 +5,27 @@ export const addUser = (user) =>{
         payload: user
     }
 }
+
+
+
+
+export const editUser = (updatedUser)=> {
+    
+    return {
+        type: "EDIT_USER",
+        updatedUser : updatedUser
+    }
+   
+    
+}
+
+
+export const deleteUser = (id) => {
+    
+    return {
+        type: "DELETE_USER",
+         payload: id
+      }
+   
+    
+}
