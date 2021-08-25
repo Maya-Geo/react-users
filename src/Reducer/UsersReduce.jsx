85ccd79295ed22f6f9aff5ef03188b8f7732c294
Maya Hagan <miyaclessa@gmail.com>
2021-08-26 08:43:10 +1200

    edit

--- src/Reducer/UsersReduce.jsx
+++ src/Reducer/UsersReduce.jsx
@@ -10,6 +10,19 @@ const UsersReduce = (state = initialState, action) => {
         case "ADD_USER":
         return {...state.users, users: [...state.users, action.payload]};
 
+        case "EDIT_USER":
+            const newUsers =  state.users.map((user) =>
+              user.id === action.payload.id ? action.payload : user
+            )
+           
+           return {...state, users: newUsers};
+
+           case "DELETE_USER":
+            const savedUsers = state.users.filter((user) => {
+              return user.id !== action.payload;
+            });
+            return {...state, users: savedUsers};
+
 
         default:
         return state;
