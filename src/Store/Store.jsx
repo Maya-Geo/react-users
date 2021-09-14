ecdf6fc981373a9209a88efd40a16dc5da98cc7a
Maya Hagan <miyaclessa@gmail.com>
2021-09-15 11:23:36 +1200

    add store

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -5,7 +5,7 @@ import {getFirebase, reactReduxFirebase} from "react-redux-firebase";
 import {getFirestore, reduxFirestore} from "redux-firestore";
 import thunk from "redux-thunk";
 import firebase from "../Firebase/Config"
-import firestore from "../Firebase/Config";
+// import firestore from "../Firebase/Config";
 
 
 
@@ -13,9 +13,9 @@ import firestore from "../Firebase/Config";
 
 const Store = createStore(UsersReduce, compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reactReduxFirebase(firebase),
-    reduxFirestore(firestore)
-
+    reduxFirestore(firebase),
+    reactReduxFirebase(firebase)
+   
 ))
  
 
