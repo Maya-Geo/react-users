b80ea2cc466118994935f3982d8d362dd1e84653
Maya Hagan <miyaclessa@gmail.com>
2021-09-17 11:10:36 +1200

    update

--- src/Actions/UserAction.jsx
+++ src/Actions/UserAction.jsx
@@ -1,13 +1,20 @@
+export const ADD_USER = "ADD_USER";
+export const Edit_User = "EDIT_USER"
+export const DELETE_USER ="DELETE_USER"
+
 export const addUser = (user) =>{
     user.id = Math.random().toString();
 
     
     return(dispatch,state,{getFirestore})=>{
-        getFirestore().collection("users").add(user).then(
+        const db = getFirestore();
+        db.collection("users").add(user).then(
             (docs)=>{
                 console.log(docs)
-            }
-        )
+            })
+
+
+        .catch((err) => console.log(err));
         // type: "ADD_USER",
         // payload: user
 
