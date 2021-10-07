9537d34d673efc4c405c575edfb1c4325cd02d5b
Maya Hagan <miyaclessa@gmail.com>
2021-10-08 00:26:37 +1300

    update

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import PrivateRoute from './ProtectedRoute'
-import { Route, BrowserRouter, NavLink } from "react-router-dom";
+import { Route, BrowserRouter,Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import Register from "./Pages/Register";
 import Login from './Pages/Login';
+import './App.css'
 
 const Router = () => {
     return (
       <BrowserRouter>
-       <NavLink to="/">Home</NavLink>
+       <div className="nav">
+       <Link className="hom" to="/">Home</Link>
+      <Link className="res" to = "/register">Register</Link>
+      <Link className="log" to = "/login">Login</Link>
+    </div>
+      
         <PrivateRoute exact path="/" component={Home} />
         <Route path="/register" component={Register} />
         <Route path="/login" component= {Login}/>
       </BrowserRouter>
     );
-    
   } // Enabling various components in application
 
 export default Router;
