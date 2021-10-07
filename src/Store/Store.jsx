216c8708fc8b54bf885fcc8af1fd6898e2921117
Maya Hagan <miyaclessa@gmail.com>
2021-10-07 23:20:58 +1300

    added firebase-reducer

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,22 +1,30 @@
-import { createStore, compose, applyMiddleware} from "redux";
+import { createStore, compose, applyMiddleware, combineReducers} from "redux";
 import UsersReduce from '../Reducer/UsersReduce';
 
-import {getFirebase, reactReduxFirebase} from "react-redux-firebase";
+
+import {getFirebase, reactReduxFirebase,firebaseReducer} from "react-redux-firebase";
+import authReducer from "../Reducer/AuthReducer";
 import {getFirestore, reduxFirestore} from "redux-firestore";
 import thunk from "redux-thunk";
 import firebase from "../Firebase/Config"
 // import firestore from "../Firebase/Config";
 
 
+let reducers = combineReducers({
+    user: UsersReduce,
+    firebase: firebaseReducer,
+    auth: authReducer,
+  });
+  
+  var Store = createStore(
+    reducers,
+    compose(
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reactReduxFirebase(firebase),
+      reduxFirestore(firebase)
+    )
+  );
 
-
-
-const Store = createStore(UsersReduce, compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(firebase),
-    reactReduxFirebase(firebase)
-   
-))
  
 
 export default Store;
