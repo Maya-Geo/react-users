cd7f212535d618ae25de08ff79d0212b7f791264
Maya Hagan <miyaclessa@gmail.com>
2021-10-14 05:29:05 +1300

    update ProtectedRoute

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -2,24 +2,33 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ components: Components, auth, isAuth, ...rest }) => {
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+  if(!auth.isLoaded) return null;
+  if(auth.uid){
+     return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return <Component {...props} />;
+      }}
+    />
+  );
+  }
+
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (auth.isLoaded && !auth.isEmpty) return <Components {...props} />;
-        else if (!auth.isLoaded) return <h1>Loading...</h1>;
-        else return <Redirect to="/login" />;
+        return <Redirect to={{ pathname: "/login" }} />;
       }}
-    />
+    /> 
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
 
-
-
-const mapStateToProps = (state) => ({
-  auth: state.firebase.auth,
-  isAuth: state.auth.isAuth,
-});
 
 export default connect(mapStateToProps)(PrivateRoute);
