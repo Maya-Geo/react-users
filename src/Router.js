48d43b59f735845d75b0ad042a098067848f01e5
Maya Hagan <miyaclessa@gmail.com>
2021-10-16 08:46:10 +1300

    removed PrivateRoute

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import PrivateRoute from './ProtectedRoute'
-import { Route, BrowserRouter,Link } from "react-router-dom";
-import Home from "./Pages/Home";
-import Register from "./Pages/Register";
+import {Route, BrowserRouter, } from "react-router-dom";
+// import PrivateRoute from "./ProtectedRoute";
+import Home from './Pages/Home';
+import Register from './Pages/Register';
 import Login from './Pages/Login';
-import './App.css'
 
-const Router = () => {
+
+ const Router = () => { 
     return (
-      <BrowserRouter>
-       <div className="nav">
-       <Link className="hom" to="/">Home</Link>
-      {/* <Link className="res" to = "/register">Register</Link> */}
-      <Link className="log" to = "/login">Login</Link>
-    </div>
-      
-        <PrivateRoute exact path="/" component={Home} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component= {Login}/>
-      </BrowserRouter>
-    );
-  } // Enabling various components in application
+        <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        
+        </BrowserRouter>
+    )
+}
+//Enabling various components in application
 
 export default Router;
+
+
+
